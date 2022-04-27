b31d0ed7daf760043b112a8e2febd0b267f60ed0
JonWLS <jwon0100@student.monash.edu>
2022-04-28 01:00:06 +0800

    Implement getBookingsService which populates Booking collection with bookings from the REST API. Implemented getCollection which returns the Booking collection.

--- src/main/java/com/example/application/data/entity/Booking/BookingCollection.java
+++ src/main/java/com/example/application/data/entity/Booking/BookingCollection.java
@@ -1,11 +1,19 @@
 package com.example.application.data.entity.Booking;
 
 import com.example.application.data.entity.HttpHelper;
+import com.example.application.data.entity.Registration.CovidTest;
+import com.example.application.data.entity.Registration.OnSiteTesting;
+import com.example.application.data.entity.Registration.OnlineTesting;
+import com.example.application.data.entity.TestingSite.TestingSite;
+import com.example.application.data.entity.User.ExpertStaff;
+import com.example.application.data.entity.User.FacilityStaff;
+import com.example.application.data.entity.User.Resident;
+import com.example.application.data.entity.User.User;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.net.http.HttpResponse;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,9 +28,73 @@ public class BookingCollection {
             System.out.println(e);
         }
     }
+    public List<Booking> getCollection() {
+        return collection;
+    }
+    public void getBookingsService() throws Exception {
+        String userUrl = "https://fit3077.com/api/v1/booking";
 
-    public void getBookingsService() throws Exception{
+        HttpResponse<String> response = new HttpHelper().getService(userUrl);
 
+        // Error checking for this sample code. You can check the status code of your request, as part of performing error handling in your assignment.
+        if (response.statusCode() != 200) {
+            throw new Exception("Please specify your API key in line 21 to continue using this sample code.");
+        }
+
+        // The GET /user endpoint returns a JSON array, so we can loop through the response as we could with a normal array/list.
+        ObjectNode[] jsonNodes = new ObjectMapper().readValue(response.body(), ObjectNode[].class);
+
+        for (ObjectNode node : jsonNodes) {
+            JsonNode userNode = node.get("customer");
+            User user = null;
+            CovidTest covidTest = null;
+
+
+            if (userNode.get("isCustomer").asBoolean())
+                user = new Resident(userNode.get("id").asText(), userNode.get("givenName").asText(), userNode.get("familyName").asText(), userNode.get("userName").asText(), userNode.get("phoneNumber").asText());
+            else if (userNode.get("isReceptionist").asBoolean())
+                user = new FacilityStaff(userNode.get("id").asText(), userNode.get("givenName").asText(), userNode.get("familyName").asText(), userNode.get("userName").asText(), userNode.get("phoneNumber").asText());
+            else if (userNode.get("isHealthcareWorker").asBoolean())
+                user = new ExpertStaff(userNode.get("id").asText(), userNode.get("givenName").asText(), userNode.get("familyName").asText(), userNode.get("userName").asText(), userNode.get("phoneNumber").asText());
+
+            if (userNode.has("address")) {
+                JsonNode testingSiteNode = node.get("testingSite");
+                TestingSite testingSite = new TestingSite(testingSiteNode.get("id").asText(),
+                        testingSiteNode.get("name").asText(),
+                        testingSiteNode.get("description").asText(),
+                        testingSiteNode.get("websiteUrl").asText(),
+                        testingSiteNode.get("phoneNumber").asText(),
+                        testingSiteNode.get("address").get("latitude").asDouble(),
+                        testingSiteNode.get("address").get("longitude").asDouble(),
+                        testingSiteNode.get("address").get("unitNumber").asInt(),
+                        testingSiteNode.get("address").get("street").asText(),
+                        testingSiteNode.get("address").get("street2").asText(),
+                        testingSiteNode.get("address").get("suburb").asText(),
+                        testingSiteNode.get("address").get("state").asText(),
+                        testingSiteNode.get("address").get("postcode").asText(),
+                        testingSiteNode.get("additionalInfo").get("facilityType").asText(),
+                        testingSiteNode.get("additionalInfo").get("openTime").asText(),
+                        testingSiteNode.get("additionalInfo").get("closeTime").asText(),
+                        testingSiteNode.get("additionalInfo").get("waitingTime").asText()
+                );
+
+                covidTest = new OnSiteTesting(testingSite);
+            } else {
+                covidTest = new OnlineTesting();
+            }
+
+            Booking booking = new FacilityBooking(
+                    node.get("id").asText(),
+                    covidTest,
+                    node.get("startTime").asText(),
+                    user,
+                    node.get("notes").asText(),
+                    node.get("status").asText(),
+                    node.get("smsPin").asText(),
+                    node.get("additionalInfo").asText());
+
+            collection.add(booking);
+        }
     }
 
     /**
