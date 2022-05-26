d8f6a8041e4b60830073e7bdc51d5871e85016c9
wpan0017 <56101902+AeroG81@users.noreply.github.com>
2022-05-26 21:18:16 +0800

    Fix bug where dialog remain after switch page

--- src/main/java/com/example/application/views/subpages/ReceptionistView.java
+++ src/main/java/com/example/application/views/subpages/ReceptionistView.java
@@ -1,41 +1,20 @@
 package com.example.application.views.subpages;
 
-import com.example.application.data.entity.BookingMethod.FacilityBookingMethod;
-import com.example.application.data.entity.TestingSite.TestingSite;
-import com.example.application.data.entity.TestingSite.TestingSiteCollection;
-import com.example.application.data.entity.User.User;
-import com.example.application.data.entity.User.UserCollection;
 import com.example.application.views.subpages.layout.*;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.combobox.ComboBox;
-import com.vaadin.flow.component.datetimepicker.DateTimePicker;
-import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
-import com.vaadin.flow.component.textfield.*;
-import com.vaadin.flow.component.textfield.TextArea;
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.BeforeLeaveEvent;
+import com.vaadin.flow.router.BeforeLeaveObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-
-import java.net.http.HttpResponse;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Route(value = "/receptionist")
 @PageTitle("OnSite")
 /**
- * This is the page for Receptionist to book user at site
+ * This is the page for Receptionist to book user at site, verify PIN, verify QR for RAT-kit, manage all booking, modify booking when called by user.
  */
-public class ReceptionistView extends VerticalLayout {
+public class ReceptionistView extends VerticalLayout implements BeforeLeaveObserver {
 
     private final Tab tabRegistration = new Tab("Registration");
     private final Tab tabVerifyPin = new Tab("Verify PIN") ;
@@ -44,6 +23,7 @@ public class ReceptionistView extends VerticalLayout {
     private final Tab tabModifyByPhone = new Tab("Modify Booking By Phone");
     private final Tabs mainTabs = new Tabs(tabRegistration,tabVerifyPin,tabScanQr,tabBookings,tabModifyByPhone);
     private final VerticalLayout mainLayout = new VerticalLayout();
+    private final NotificationDialog notification = new NotificationDialog();
 
     /**
      * Populating page with components based on tab selected
@@ -68,13 +48,18 @@ public class ReceptionistView extends VerticalLayout {
                 mainLayout.add(new ModifyBookingByPhoneLayout());
             }
         });
-        NotificationDialog notification = new NotificationDialog();
         if (notification.newNotificationExist())
             notification.open();
+
         setMargin(false);
         setPadding(true);
         setJustifyContentMode(JustifyContentMode.CENTER);
         add(new ProfileAvatarLayout(),mainTabs,mainLayout);
     }
-
+    @Override
+    public void beforeLeave(BeforeLeaveEvent event) {
+        BeforeLeaveEvent.ContinueNavigationAction action = event.postpone();
+        notification.close();
+        action.proceed();
+    }
 }
